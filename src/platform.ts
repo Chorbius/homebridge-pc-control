aca6a56d7922977ff556d2b7809b653b0f79feee
Sleepy9000 <jkozak2002@gmail.com>
2024-07-09 03:08:30 -0400

    i did some stuff and updated the version

--- src/platform.ts
+++ src/platform.ts
@@ -6,11 +6,9 @@ import {
   PlatformConfig,
   Service,
   Characteristic,
-  CharacteristicValue,
 } from 'homebridge';
-import axios from 'axios';
-import wol from 'wake_on_lan';
 import { PLUGIN_NAME, PLATFORM_NAME } from './settings';
+import { PCControlAccessory } from './platformAccessory';
 
 export class PCControlPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service;
@@ -63,62 +61,6 @@ export class PCControlPlatform implements DynamicPlatformPlugin {
       accessory.context.device = device;
       new PCControlAccessory(this, accessory);
       this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-    }
-  }
-}
-
-class PCControlAccessory {
-  private service: Service;
-
-  constructor(
-    private readonly platform: PCControlPlatform,
-    private readonly accessory: PlatformAccessory,
-  ) {
-    this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
-
-    this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.displayName);
-
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))
-      .onGet(this.getOn.bind(this));
-  }
-
-  async setOn(value: CharacteristicValue) {
-    const device = this.accessory.context.device;
-
-    if (value as boolean) {
-      this.platform.log.info('Turning on PC');
-      wol.wake(device.macAddress, (error) => {
-        if (error) {
-          this.platform.log.error('Failed to wake PC:', error);
-        } else {
-          this.platform.log.info('Wake-on-LAN packet sent');
-        }
-      });
-    } else {
-      this.platform.log.info('Turning off PC');
-      try {
-        await axios.post(`http://${device.ipAddress}:${device.port}/shutdown`);
-        this.platform.log.info('Shutdown command sent to PC');
-      } catch (error) {
-        this.platform.log.error('Failed to shutdown PC:', error);
-      }
-    }
-  }
-
-  async getOn(): Promise<CharacteristicValue> {
-    const device = this.accessory.context.device;
-
-    try {
-      const response = await axios.get(`http://${device.ipAddress}:${device.port}/status`);
-      return response.data.isOn;
-    } catch (error) {
-      this.platform.log.error('Failed to get PC status:', error);
-      return false;
     }
   }
 }
