fd8c105f15e7422d109107bb7d49bb82b5b626f9
Sleepy9000 <jkozak2002@gmail.com>
2024-07-09 01:48:54 -0400

    pressed save on something else in settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,17 @@
+// eslint-disable-next-line
+// @ts-ignore
+import { pluginAlias as platformName } from '../config.schema.json';
+
+// eslint-disable-next-line
+// @ts-ignore
+import { name as pluginName } from '../package.json';
+
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'PCControl';
+export const PLATFORM_NAME = platformName;
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@chorb/homebridge-pc-control';
+export const PLUGIN_NAME = pluginName;
